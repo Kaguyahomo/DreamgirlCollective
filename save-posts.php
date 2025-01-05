e66027ddd9146d64bfa710f02daea33a8e03ea4f
Kaguyahomo <Kaguyahomo@users.noreply.github.com>
2025-01-05 02:17:23 +0000

    deploy: 8bdacba029882ce90b2b81dca0d7842d447c389e

--- save-posts.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $jsonData = file_get_contents('php://input');
-    file_put_contents('posts.json', $jsonData);
-    echo json_encode(['status' => 'success']);
-} else {
-    echo json_encode(['status' => 'error', 'message' => 'Invalid request']);
-}
-?>
